3ec41135d5e24389998275b2652fc09b952f3bc4
J. Rivero <jrivero@rocketmail.com>
2014-02-04 19:11:47 -0800

    Finished part 7
    added MainActivity, it needs the StockInfoActivity class.

--- gen/com/example/stockquote/R.java
+++ gen/com/example/stockquote/R.java
@@ -10,6 +10,11 @@ package com.example.stockquote;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aero_blue=0x7f040000;
+        public static final int black=0x7f040002;
+        public static final int egg_shell=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +22,62 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int stock_list_text_size=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int TableLayout1=0x7f090000;
+        public static final int action_settings=0x7f09000d;
+        public static final int deleteStocksButton=0x7f09000a;
+        public static final int enterStockSymbolButton=0x7f090004;
+        public static final int enterSymbolTextView=0x7f090002;
+        public static final int quoteFromWebButton=0x7f09000c;
+        public static final int stockQuoteButton=0x7f09000b;
+        public static final int stockScrollView=0x7f090008;
+        public static final int stockSymbolEditText=0x7f090003;
+        public static final int stockSymbolTextViewRow=0x7f090006;
+        public static final int tableRow1=0x7f090001;
+        public static final int tableRow2=0x7f090005;
+        public static final int tableRow3=0x7f090007;
+        public static final int tableRow4=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int stock_quote_row=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int cancel=0x7f060007;
+        public static final int change=0x7f060015;
+        public static final int change_stock_symbol=0x7f060005;
+        public static final int days_range=0x7f060016;
+        public static final int delete_all_symbols=0x7f060009;
+        public static final int enter_stock_symbol=0x7f060003;
+        public static final int get_stock_quote=0x7f06000a;
+        public static final int get_stock_quote_from_web=0x7f06000b;
+        public static final int invalid_stock_symbol=0x7f06000c;
+        public static final int last_trade_price_only=0x7f060014;
+        public static final int missing_stock_symbol=0x7f060008;
+        public static final int ok=0x7f060006;
+        public static final int stock_company_name=0x7f06000e;
+        public static final int stock_days_high=0x7f060012;
+        public static final int stock_days_low=0x7f060011;
+        public static final int stock_symbol=0x7f060002;
+        public static final int stock_symbol_list=0x7f060004;
+        public static final int stock_year_high=0x7f060010;
+        public static final int stock_year_low=0x7f06000f;
+        public static final int title_activity_stock_info=0x7f060013;
+        /**  URL and XML strings 
+         */
+        public static final int yahoo_stock_url=0x7f06000d;
     }
     public static final class style {
         /** 
@@ -59,10 +101,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/example/stockquote/MainActivity.java
+++ src/com/example/stockquote/MainActivity.java
@@ -1,15 +1,169 @@
 package com.example.stockquote;
 
+import java.util.Arrays;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	public final static String STOCK_SYMBOL = "com.example.stockquote.STOCK";
+	private SharedPreferences stockSymbolsEntered;
+	private TableLayout stockTableScrollView;
+	private EditText stockSymbolEditText;
+	
+	Button enterStockSymbolButton;
+	Button deleteStocksButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		stockSymbolsEntered = getSharedPreferences("stockList", MODE_PRIVATE);
+		stockTableScrollView = (TableLayout) findViewById(R.id.stockScrollView);
+		stockSymbolEditText = (EditText) findViewById(R.id.stockSymbolEditText);
+		
+		enterStockSymbolButton = (Button)findViewById(R.id.enterStockSymbolButton);
+		deleteStocksButton = (Button)findViewById(R.id.deleteStocksButton);
+		
+		enterStockSymbolButton.setOnClickListener(enterStockButtonListener);
+		deleteStocksButton.setOnClickListener(deleteStocksButtonListenerl);
+		updateSavedStockList(null);
+
+	}
+	
+	public OnClickListener enterStockButtonListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			
+			if(stockSymbolEditText.getText().length()>0) {
+				saveStockSymbol(stockSymbolEditText.getText().toString());
+				stockSymbolEditText.setText("");
+				
+				// close keyboard
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(stockSymbolEditText.getWindowToken(), 0);
+			} else {
+				// alert dialog box
+				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+				builder.setTitle(R.string.invalid_stock_symbol);
+				builder.setPositiveButton(R.string.ok, null);
+				builder.setMessage(R.string.missing_stock_symbol);
+				AlertDialog theAlertDialog = builder.create();
+				theAlertDialog.show();
+			}
+		}
+	};
+	
+	public OnClickListener deleteStocksButtonListenerl = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			deleteAllStocks();
+			SharedPreferences.Editor preferencesEditor = stockSymbolsEntered.edit();
+			preferencesEditor.clear();
+			preferencesEditor.commit();
+		}
+		
+	};
+	
+	private void deleteAllStocks() {
+		stockTableScrollView.removeAllViews();
+	}
+
+	private void updateSavedStockList(String newStockSymbol) {
+		
+		String[] stocks = stockSymbolsEntered.getAll().keySet().toArray(new String[0]);
+		Arrays.sort(stocks, String.CASE_INSENSITIVE_ORDER);
+		
+		if(newStockSymbol != null) {
+			insertStockInScrollView(newStockSymbol, Arrays.binarySearch(stocks, newStockSymbol));
+		} else {
+			for(int i = 0; i < stocks.length; i++)
+				insertStockInScrollView(stocks[i], i);
+		}
+	}
+	
+	private void insertStockInScrollView(String newStockSymbol, int binarySearch) {
+		LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		//set or create stock row in second frame
+		View newStockRow =  inflator.inflate(R.layout.stock_quote_row, null);
+		
+		TextView newStockTextView = (TextView) newStockRow.findViewById(R.id.stockSymbolTextViewRow);
+		newStockTextView.setText(newStockSymbol);
+		
+		Button stockQuoteButton = (Button) newStockRow.findViewById(R.id.stockQuoteButton);
+		stockQuoteButton.setOnClickListener(getStockActivityListener);
+		
+		Button quoteFromWebButton = (Button) newStockRow.findViewById(R.id.quoteFromWebButton);
+		quoteFromWebButton.setOnClickListener(getStockFromWebSiteListener);
+		
+		stockTableScrollView.addView(newStockRow);
+	}
+	
+	public OnClickListener getStockFromWebSiteListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+			TableRow tableRow = (TableRow) v.getParent();
+			TextView stockTextView = (TextView) tableRow.findViewById(R.id.stockSymbolTextViewRow);
+			String stockSymbol = stockTextView.getText().toString();
+			
+			String stockURL = getString(R.string.yahoo_stock_url) + stockSymbol; 
+			Intent getStockWebPage = new Intent(Intent.ACTION_VIEW, Uri.parse(stockURL));
+			
+			startActivity(getStockWebPage);
+			
+			
+		}
+
+	};
+	
+	public OnClickListener getStockActivityListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			TableRow tableRow = (TableRow) v.getParent();
+			TextView stockTextView = (TextView) tableRow.findViewById(R.id.stockSymbolTextViewRow);
+			String stockSymbol = stockTextView.getText().toString();
+			Intent intent = new Intent(MainActivity.this, StockInfoActivity.class);
+			intent.putExtra(STOCK_SYMBOL, stockSymbol);
+			startActivity(intent);
+		}
+		
+	};
+
+
+	private void saveStockSymbol(String newStock) {
+		SharedPreferences.Editor preferencesEditor = stockSymbolsEntered.edit();
+		preferencesEditor.putString(newStock, newStock);
+		preferencesEditor.commit();
+		
+		String isTheStockNew = stockSymbolsEntered.getString(newStock,  null);
+		
+		if(isTheStockNew==null) {
+			updateSavedStockList(newStock);
+		}
+
 	}
 
 	@Override
